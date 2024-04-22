aec98320273153e078d3107b8702b57ce752b77e
Chinthurajendran <chinthurajendran1512@gmail.com>
2024-04-22 10:13:12 +0530

    hide credentials

--- main/settings.py
+++ main/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@etj!d+7ywk+qszlt8heo(a64s%(8+6j73*k0cztuvsnk-88g'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
